ee9690622eb0bd763f1a7339320d0c13ebf01230
Yashy <yashwit.po@gmail.com>
2024-05-16 02:07:03 +0800

    Delete Yash/src/Components directory

--- Yash/src/Components/About/AboutPage.tsx
+++
@@ -1,9 +0,0 @@
-import IntroSection from "./IntroSection";
-
-export default function AboutPage() {
-  return (
-    <div>
-      <IntroSection />
-    </div>
-  );
-}
--- Yash/src/Components/moviestv/reviews/ComponentMap.tsx
+++
@@ -1,6 +0,0 @@
-import InglouriousBasterds from "./InglouriousBasterds";
-import { ComponentMapType } from "../../../utils/typedefs/MoviesTVCustomTypes";
-
-export const ComponentMap: ComponentMapType = {
-  InglouriousBasterds,
-};
