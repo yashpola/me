eedd0a443956a9e8139d11923851c6739638b29d
yashpola <yashwit.po@gmail.com>
2024-05-28 19:50:18 +0800

    Change movie/tv review urls to deployed link

--- Yash/src/components/moviestv/Dashboard.tsx
+++ Yash/src/components/moviestv/Dashboard.tsx
@@ -75,7 +75,7 @@ export default function Dashboard({
               <>
                 <div key={idx} className="posters-dashboard">
                   <a
-                    href={`http://localhost:5173/${entry.name.replace(
+                    href={`https://yashyswebsitev2.vercel.app/movies/${entry.name.replace(
                       / /g,
                       ""
                     )}`}
