85c4eeb571ca93796b2cfc96cc6bd919add076d5
yashpola <yashwit.po@gmail.com>
2025-01-05 14:37:01 +0800

    Fix type errors in course & tv reviews

--- Yash/src/components/learnings/coursereviews/NEX3003.tsx
+++ Yash/src/components/learnings/coursereviews/NEX3003.tsx
@@ -2,5 +2,6 @@ import CoursePage from "../CoursePage";
 import { CoursePostType } from "../../../utils/typedefs/LearningsCustomTypes";
 
 export default function NEX3003({ course }: { course: CoursePostType }) {
-  return <CoursePage course={course} />;
+  const images = undefined;
+  return <CoursePage course={course} images={images} />;
 }
