d67988825b89e1280128b8463d1dec399f9ee770
yashpola <yashwit.po@gmail.com>
2025-04-26 11:09:02 +0800

    Fix env var get issues for vite

--- Yash/vite.config.ts
+++ Yash/vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  assetsInclude: ["**/*.JPG"],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    assetsInclude: ["**/*.JPG"],
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+  };
 });
